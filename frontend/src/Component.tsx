2e04f2f2f9c00b473a65da7ac0bfb0b601afb751
Nate Wong <natewong1@gmail.com>
2023-09-07 16:52:39 -0400

    upload todays work

---
+++ frontend/src/Component.tsx
@@ -0,0 +1,3 @@
+export default function Component() {
+  return <div className="test">Hello world</div>;
+}
---
+++ frontend/src/test.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import App from "./App.tsx";
+
+ReactDOM.render(<App />, document.getElementById("root"));
