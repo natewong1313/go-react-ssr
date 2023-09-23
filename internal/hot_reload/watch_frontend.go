1bed8f3766ef9d434af84b32c7750da6db5db2b0
Nate Wong <natewong1@gmail.com>
2023-09-23 09:10:00 -0400

    Tailwind improvements

--- internal/hot_reload/watch_frontend.go
+++ internal/hot_reload/watch_frontend.go
@@ -31,12 +31,16 @@ func WatchForFileChanges() {
 			if event.Op.String() != "CHMOD" && !strings.Contains(event.Name, "gossr-temporary") {
 				filePath := event.Name
 				logger.L.Info().Msgf("File changed: %s, reloading", filePath)
+				// Store the routes that need to be reloaded
 				var routeIDS []string
-				// Check if its the global css file thats gotten updated
-				if utils.GetFullFilePath(filePath) == utils.GetFullFilePath(config.C.GlobalCSSFilePath) {
+				switch {
+				case globalCSSFileUpdated(filePath):
 					react_renderer.BuildGlobalCSSFile()
 					routeIDS = react_renderer.GetAllRouteIDS()
-				} else {
+				case needsTailwindRecompile(filePath):
+					react_renderer.BuildGlobalCSSFile()
+					fallthrough
+				default:
 					// Get all route ids that use that file or have it as a dependency
 					routeIDS = react_renderer.GetRouteIDSWithFile(filePath)
 				}
@@ -48,6 +52,24 @@ func WatchForFileChanges() {
 			logger.L.Err(err).Msg("Error watching file")
 		}
 	}
+}
+
+func globalCSSFileUpdated(filePath string) bool {
+	return utils.GetFullFilePath(filePath) == utils.GetFullFilePath(config.C.GlobalCSSFilePath)
+}
+
+// Check if tailwind is enabled and if the file is a react file.
+func needsTailwindRecompile(filePath string) bool {
+	if config.C.TailwindConfigPath == "" {
+		return false
+	}
+	fileTypes := []string{".tsx", ".ts", ".jsx", ".js"}
+	for _, fileType := range fileTypes {
+		if strings.HasSuffix(filePath, fileType) {
+			return true
+		}
+	}
+	return false
 }
 
 func watchFilesInDir(path string, fi os.FileInfo, err error) error {
