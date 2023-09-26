3127b98bd9b75b03ec96d167b4066f4c36185bfe
Nate Wong <natewong1@gmail.com>
2023-09-25 20:50:04 -0400

    Update examples and tests

--- config/config_test.go
+++ config/config_test.go
@@ -2,6 +2,8 @@ package config
 
 import (
 	"testing"
+
+	"github.com/natewong1313/go-react-ssr/internal/utils"
 )
 
 func TestLoad(t *testing.T) {
@@ -26,18 +28,28 @@ func TestLoad(t *testing.T) {
 		{"test with PropsStructsPath that doesnt exist", args{Config{
 			PropsStructsPath: "test",
 		}}, true},
+		{"test with tailwind config and global css file", args{Config{
+			FrontendDir:        "../examples/frontend-tailwind/src",
+			TailwindConfigPath: "../examples/frontend-tailwind/tailwind.config.js",
+			GlobalCSSFilePath:  "../examples/frontend-tailwind/src/Main.css",
+		}}, false},
 		{"test with tailwind config but no global css file", args{Config{
-			TailwindConfigPath: "../examples/frontend/package.json",
+			TailwindConfigPath: "../examples/frontend-tailwind/tailwind.config.js",
 		}}, true},
 		{"test with tailwind config but global css file that doesnt exist", args{Config{
-			FrontendDir:        "../examples/frontend/src",
-			TailwindConfigPath: "../examples/frontend/package.json", //fuck it we ball
+			FrontendDir:        "../examples/frontend-tailwind/src",
+			TailwindConfigPath: "../examples/frontend-tailwind/tailwind.config.js",
 			GlobalCSSFilePath:  "test",
 		}}, true},
 		{"test with tailwind config that doesnt exist", args{Config{
-			FrontendDir:        "../examples/frontend/src",
+			FrontendDir:        "../examples/frontend-tailwind/src",
 			TailwindConfigPath: "test",
-			GlobalCSSFilePath:  "../examples/frontend/src/Home.css",
+			GlobalCSSFilePath:  "../examples/frontend-tailwind/src/Main.css",
+		}}, true},
+		{"test with tailwind not installed", args{Config{
+			FrontendDir:        "../examples/frontend/src",
+			TailwindConfigPath: "../examples/frontend/src",
+			GlobalCSSFilePath:  "../examples/frontend-tailwind/src/Main.css",
 		}}, true},
 	}
 	for _, tt := range tests {
@@ -65,6 +77,24 @@ func TestCheckPathExists(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := checkPathExists(tt.args); got != tt.want {
 				t.Errorf("checkPathExists() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+func TestCheckTailwindInstalled(t *testing.T) {
+	tests := []struct {
+		name       string
+		workingDir string
+		want       bool
+	}{
+		{"test tailwind installed", utils.GetFullFilePath("../examples/frontend-tailwind/src"), true},
+		{"test tailwind not installed", utils.GetFullFilePath("."), false},
+	}
+	for _, tt := range tests {
+		Load(Config{FrontendDir: tt.workingDir})
+		t.Run(tt.name, func(t *testing.T) {
+			if got := checkTailwindInstalled(); got != tt.want {
+				t.Errorf("checkTailwindInstalled() = %v, want %v", got, tt.want)
 			}
 		})
 	}
