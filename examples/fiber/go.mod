2a9213befe9e7066f1460ec7e2cb43768e89fbb9
Nate Wong <natewong1@gmail.com>
2023-09-27 20:45:57 -0400

    Use goja for runtime instead of node

--- examples/fiber/go.mod
+++ examples/fiber/go.mod
@@ -4,29 +4,30 @@ go 1.21.0
 
 require (
 	github.com/gofiber/fiber/v2 v2.49.2
-	github.com/natewong1313/go-react-ssr v0.0.3
+	github.com/natewong1313/go-react-ssr v0.0.693
 )
 
 require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
 	github.com/buger/jsonparser v1.1.1 // indirect
 	github.com/creasty/defaults v1.7.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/evanw/esbuild v0.19.3 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/google/uuid v1.3.1 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
-	github.com/klauspost/compress v1.16.7 // indirect
+	github.com/klauspost/compress v1.17.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
-	github.com/rivo/uniseg v0.2.0 // indirect
-	github.com/rs/zerolog v1.30.0 // indirect
-	github.com/stretchr/testify v1.8.3 // indirect
+	github.com/rivo/uniseg v0.4.4 // indirect
+	github.com/rs/zerolog v1.31.0 // indirect
 	github.com/tkrajina/go-reflector v0.5.6 // indirect
 	github.com/tkrajina/typescriptify-golang-structs v0.1.10 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasthttp v1.49.0 // indirect
+	github.com/valyala/fasthttp v1.50.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
 	golang.org/x/sys v0.12.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- react_renderer/build_server.go
+++ react_renderer/build_server.go
@@ -1,11 +1,10 @@
 package react_renderer
 
 import (
-	"bytes"
 	"fmt"
-	"os/exec"
 	"strings"
 
+	"github.com/dop251/goja"
 	esbuildApi "github.com/evanw/esbuild/pkg/api"
 	"github.com/natewong1313/go-react-ssr/config"
 )
@@ -13,12 +12,19 @@ import (
 func buildForServer(reactFilePath, props string, c chan<- ServerBuildResult) {
 	buildResult := esbuildApi.Build(esbuildApi.BuildOptions{
 		Stdin: &esbuildApi.StdinOptions{
+			// const App = require("%s").default;
+			// const props = %s
+			// console.log(renderToString(<App {...props} />));
 			Contents: fmt.Sprintf(`import { renderToString } from "react-dom/server";
 			import React from "react";
 	
-			const App = require("%s").default;
-			const props = %s
-			console.log(renderToString(<App {...props} />));
+
+			function render() {
+				const App = require("%s").default;
+				const props = %s
+				return renderToString(<App {...props} />);
+			  }
+			  globalThis.render = render;
 		  `, reactFilePath, props),
 			Loader:     esbuildApi.LoaderTSX,
 			ResolveDir: config.C.FrontendDir,
@@ -51,14 +57,114 @@ func buildForServer(reactFilePath, props string, c chan<- ServerBuildResult) {
 		}
 	}
 
-	// exec code with node
-	node := exec.Command("node", "-e", string(buildResult.OutputFiles[0].Contents))
-	var outb bytes.Buffer
-
-	node.Stdout = &outb
-	if err := node.Run(); err != nil {
+	vm := goja.New()
+	_, err := vm.RunString(`function TextEncoder() {
+	}
+	
+	TextEncoder.prototype.encode = function (string) {
+	  var octets = [];
+	  var length = string.length;
+	  var i = 0;
+	  while (i < length) {
+		var codePoint = string.codePointAt(i);
+		var c = 0;
+		var bits = 0;
+		if (codePoint <= 0x0000007F) {
+		  c = 0;
+		  bits = 0x00;
+		} else if (codePoint <= 0x000007FF) {
+		  c = 6;
+		  bits = 0xC0;
+		} else if (codePoint <= 0x0000FFFF) {
+		  c = 12;
+		  bits = 0xE0;
+		} else if (codePoint <= 0x001FFFFF) {
+		  c = 18;
+		  bits = 0xF0;
+		}
+		octets.push(bits | (codePoint >> c));
+		c -= 6;
+		while (c >= 0) {
+		  octets.push(0x80 | ((codePoint >> c) & 0x3F));
+		  c -= 6;
+		}
+		i += codePoint >= 0x10000 ? 2 : 1;
+	  }
+	  return octets;
+	};
+	
+	function TextDecoder() {
+	}
+	
+	TextDecoder.prototype.decode = function (octets) {
+	  var string = "";
+	  var i = 0;
+	  while (i < octets.length) {
+		var octet = octets[i];
+		var bytesNeeded = 0;
+		var codePoint = 0;
+		if (octet <= 0x7F) {
+		  bytesNeeded = 0;
+		  codePoint = octet & 0xFF;
+		} else if (octet <= 0xDF) {
+		  bytesNeeded = 1;
+		  codePoint = octet & 0x1F;
+		} else if (octet <= 0xEF) {
+		  bytesNeeded = 2;
+		  codePoint = octet & 0x0F;
+		} else if (octet <= 0xF4) {
+		  bytesNeeded = 3;
+		  codePoint = octet & 0x07;
+		}
+		if (octets.length - i - bytesNeeded > 0) {
+		  var k = 0;
+		  while (k < bytesNeeded) {
+			octet = octets[i + k + 1];
+			codePoint = (codePoint << 6) | (octet & 0x3F);
+			k += 1;
+		  }
+		} else {
+		  codePoint = 0xFFFD;
+		  bytesNeeded = octets.length - i;
+		}
+		string += String.fromCodePoint(codePoint);
+		i += bytesNeeded + 1;
+	  }
+	  return string
+	};
+	(function(global) {
+		'use strict';
+		if (!global.console) {
+		  global.console = {};
+		}
+		var con = global.console;
+		var prop, method;
+		var dummy = function() {};
+		var properties = ['memory'];
+		var methods = ('assert,clear,count,debug,dir,dirxml,error,exception,group,' +
+		   'groupCollapsed,groupEnd,info,log,markTimeline,profile,profiles,profileEnd,' +
+		   'show,table,time,timeEnd,timeline,timelineEnd,timeStamp,trace,warn,timeLog,trace').split(',');
+		while (prop = properties.pop()) if (!con[prop]) con[prop] = {};
+		while (method = methods.pop()) if (!con[method]) con[method] = dummy;
+	  })(typeof window === 'undefined' ? this : window);`)
+	if err != nil {
 		c <- ServerBuildResult{Error: err}
 		return
 	}
-	c <- ServerBuildResult{HTML: outb.String(), CSS: css}
+	_, err = vm.RunString(string(buildResult.OutputFiles[0].Contents))
+	if err != nil {
+		c <- ServerBuildResult{Error: err}
+		return
+	}
+	render, ok := goja.AssertFunction(vm.Get("render"))
+	if !ok {
+		c <- ServerBuildResult{Error: err}
+		return
+	}
+	res, err := render(goja.Undefined())
+	if err != nil {
+		c <- ServerBuildResult{Error: err}
+		return
+	}
+	c <- ServerBuildResult{HTML: res.String(), CSS: css}
 }
